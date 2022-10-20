9ca58a051170e93f88e283e23de8f7a575030c5e
yangjiarui <245837941@qq.com>
2022-10-20 17:32:01 +0800

    1.1 编写了电子吉他版本 增加了两首歌曲

--- guitar.py
+++ guitar.py
@@ -7,24 +7,29 @@ k = PyKeyboard()
 interval = 0.25 + random.uniform(-0.03, 0.03)
 
 
-def tap(tone):
-    k.press_key(tone)
-    time.sleep(0.05)
-    k.release_key(tone)
-
-
 def play():
+    buf = []
     for x in range(0, len(tones) - 1, 2):
         code = tones[x] + tones[x + 1]
         tone = dec(code)
-        if tone == -1:
+        if tone == 1:
+            if buf:
+                k.release_key(buf)
+                time.sleep(0.03)
+                buf = []
+        elif tone == -1:
             time.sleep(interval)
         else:
-            if tone == 1:
-                a = 1
-            else:
-                tap(tone)
-                time.sleep(interval)
+            if buf:
+                k.release_key(buf)
+                time.sleep(0.03)
+                buf = []
+            k.press_key(tone)
+            buf = tone
+            time.sleep(interval)
+    if buf:
+        k.release_key(buf)
+        time.sleep(0.03)
 
 
 time.sleep(2)
@@ -33,7 +38,7 @@ time.sleep(2)
 # time.sleep(3)
 # k.press_keys(['command', 'shift', '4'])
 
-fo = open('thankyou', 'r+')
+fo = open('DaBeiZhou', 'r+')
 tones = fo.read()
 fo.close()
 
